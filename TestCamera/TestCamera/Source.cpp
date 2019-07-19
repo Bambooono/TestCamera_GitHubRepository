5b8059601e6233dadaa2a6f1da8b71d60cef01b8
InsideBamboo <51089177+InsideBamboo@users.noreply.github.com>
2019-07-19 23:41:40 +0900

    Red image.

--- TestCamera/TestCamera/Source.cpp
+++ TestCamera/TestCamera/Source.cpp
@@ -3,11 +3,13 @@
 
 int main(void) {
 
-	cv::Mat image(cv::Size(640, 480), CV_8UC1);
+	cv::Mat image(cv::Size(640, 480), CV_8UC3);
 
 	image = cv::Scalar(0, 0, 255);
 
 	cv::namedWindow("test");
+
+	cv::imshow("test", image);
 
 	cv::waitKey();
 
